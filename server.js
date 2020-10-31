4e5bef0bd2582cfaa18950053801c544afd7c43a
ou1z <59099381+ou1z@users.noreply.github.com>
2020-10-31 16:56:19 -0400

    Update server.js

--- server.js
+++ server.js
@@ -10,8 +10,14 @@ const server = new WebSocket.Server({ port: port || 3000 });
 const selfbot = require('selfbot.js');
 const bot = new selfbot.Client();
 
+
+let LogChannelID 
+
 bot.on('message', (msg) => {
     let json = JSON.stringify({author: msg.author.username, message: msg.content})
+    console.log(msg.channel.id)
+    console.log(LogChannelID)
+    if(msg.channel.id != LogChannelID && msg.channel.type != 'dm') return
 
     server.clients.forEach(client => {
         client.send(json)
@@ -30,11 +36,24 @@ server.on('connection', (client) => {
             let msg = obj['Content']
             let server = obj['ServerId']
             let channel = obj['ChannelId']
+            let userid = obj['UserId']
+            let type = obj['Type']
 
-            let serverobj = bot.guilds.get(server)
-            let channelobj = serverobj.channels.get(channel)
+            if (type === 'Server') {
+                let serverobj = bot.guilds.get(server)
+                let channelobj = serverobj.channels.get(channel)
 
-            channelobj.send(msg)
+                channelobj.send(msg)
+                LogChannelID = channel;
+            }
+            else if (type === 'DM') {
+                LogChannelID = userid
+                bot.fetchUser(userid).then(user => {
+                    user.send(msg)
+                })
+            }
+
+            
         }
     })
 })
